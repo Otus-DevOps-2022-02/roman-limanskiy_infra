bf991ff97a69198bda7940dbc9ec28bdf747dac2
Roman Limanskiy <rulimanskiy@gmail.com>
2022-04-11 19:43:53 +0500

    Add all terraform files

---
+++ terraform/lb.tf
@@ -0,0 +1,36 @@
+resource "yandex_lb_target_group" "lb-target-group" {
+  name      = "lb-target-group"
+  region_id = "ru-central1"
+
+  dynamic "target" {
+    for_each = yandex_compute_instance.app.*.network_interface.0.ip_address
+    content {
+      subnet_id = var.subnet_id
+      address   = target.value
+    }
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "lb" {
+  name = "load-balancer"
+  folder_id = var.folder_id
+
+  listener {
+    name = "my-listener"
+    port = 8080
+    target_port = "9292"
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }  
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.lb-target-group.id
+    
+    healthcheck {
+      name = "tcp"
+      tcp_options {
+        port = 9292
+      }
+    }
+  }
+}
