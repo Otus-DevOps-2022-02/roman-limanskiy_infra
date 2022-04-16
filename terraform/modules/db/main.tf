2a15f12f083524c64f9b314a278b811adec8744b
Roman Limanskiy <rulimanskiy@gmail.com>
2022-04-16 15:13:59 +0500

    Add files to terraform/modules/

---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,28 @@
+resource "yandex_compute_instance" "db" {
+  name     = "reddit-db"
+  hostname = "reddit-db"
+  labels = {
+    tags = "reddit-db"
+  }
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.db_disk_image
+      size     = 11
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
